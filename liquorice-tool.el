12e43ca141f81075978b5979dc29a0c3edcf1543
Matti Hänninen <matti@mjhanninen.com>
2015-05-10 09:59:05 +0300

    Tag color spaces, improve conversions
    Add tag that indicates the color space. Convert automatically between
    the color spaces as necessary.

--- liquorice-tool.el
+++ liquorice-tool.el
@@ -40,12 +40,7 @@
 ;;; D65 reference white:
 ;;; D65 | 0.95047 | 1.00000 | 1.08883
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; String conversions
-
-(defun rgb-to-string (rgb)
-  (cl-destructuring-bind (r g b) rgb
-    (liquorice-rgb r g b)))
 
 (defun string-to-rgb (color)
   (let ((triplet (x-color-values color)))
@@ -54,14 +49,15 @@
                             (= (length color) 10))
                        17821440.0
                      65280.0)))
-        (mapcar (lambda (x)
-                  (/ (float x) denom))
-                triplet)))))
+        (cons :rgb
+              (mapcar (lambda (x)
+                             (/ (float x) denom))
+                           triplet))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Primitive conversions between color spaces
 
-;;; sRGB-tp-XYZ
+;;;; Conversions between sRGB and XYZ spaces
 
 (defconst d65-ref-x 0.95047)
 
@@ -84,109 +80,154 @@
     (* 12.92 v)))
 
 (defun srgb-to-xyz (rgb)
-  (cl-destructuring-bind (r g b) (mapcar #'srgb-compand rgb)
-    (let* ((x (+ (* r 0.4124564)
-                 (* g 0.3575761)
-                 (* b 0.1804375)))
-           (y (+ (* r 0.2126729)
-                 (* g 0.7151522)
-                 (* b 0.0721750)))
-           (z (+ (* r 0.0193339)
-                 (* g 0.1191920)
-                 (* b 0.9503041))))
-      (list x y z))))
+  (if (eq (car rgb) :rgb)
+      (cl-destructuring-bind (r g b) (mapcar #'srgb-compand
+                                             (cdr rgb))
+        (let* ((x (+ (* r 0.4124564)
+                     (* g 0.3575761)
+                     (* b 0.1804375)))
+               (y (+ (* r 0.2126729)
+                     (* g 0.7151522)
+                     (* b 0.0721750)))
+               (z (+ (* r 0.0193339)
+                     (* g 0.1191920)
+                     (* b 0.9503041))))
+          (list :xyz x y z)))
+    (error "Expected a RGB triplet")))
 
 (defun xyz-to-srgb (xyz)
-  (cl-destructuring-bind (x y z) xyz
-    (let* ((r (srgb-expand
-               (+ (* x  3.2404542)
-                  (* y -1.5371385)
-                  (* z -0.4985314))))
-           (g (srgb-expand
-               (+ (* x -0.9692660)
-                  (* y  1.8760108)
-                  (* z  0.0415560))))
-           (b (srgb-expand
-               (+ (* x  0.0556434)
-                  (* y -0.2040259)
-                  (* z  1.0572252)))))
-      (list r g b))))
+  (if (eq (car xyz) :xyz)
+      (cl-destructuring-bind (x y z) (cdr xyz)
+        (let* ((r (srgb-expand
+                   (+ (* x  3.2404542)
+                      (* y -1.5371385)
+                      (* z -0.4985314))))
+               (g (srgb-expand
+                   (+ (* x -0.9692660)
+                      (* y  1.8760108)
+                      (* z  0.0415560))))
+               (b (srgb-expand
+                   (+ (* x  0.0556434)
+                      (* y -0.2040259)
+                      (* z  1.0572252)))))
+          (list :rgb r g b)))
+    (error "Expected a XYZ triplet")))
+
+;;;; Conversions between XYZ and Luv spaces
 
 (defun xyz-to-luv (xyz)
-  (cl-destructuring-bind (x y z) xyz
-    (let* ((y-rel (/ y d65-ref-y))
-           (L (if (> y-rel cie-epsilon)
-                  (- (* 116.0
-                        (expt y-rel (/ 1.0 3.0)))
-                     16.0)
-                (* cie-kappa y-rel)))
-           (denom-val (+ x
-                         (* 15.0 y)
-                         (* 3.0 z)))
-           (denom-ref (+ d65-ref-x
-                         (* 15.0 d65-ref-y)
-                         (* 3.0 d65-ref-z)))
-           (u (* L 13.0 4.0
-                 (- (/ x denom-val)
-                    (/ d65-ref-x denom-ref))))
-           (v (* L 13.0 9.0
-                 (- (/ y denom-val)
-                    (/ d65-ref-y denom-ref)))))
-      (list L u v))))
+  (if (eq (car xyz) :xyz)
+      (cl-destructuring-bind (x y z) (cdr xyz)
+        (let* ((y-rel (/ y d65-ref-y))
+               (L (if (> y-rel cie-epsilon)
+                      (- (* 116.0
+                            (expt y-rel (/ 1.0 3.0)))
+                         16.0)
+                    (* cie-kappa y-rel)))
+               (denom-val (+ x
+                             (* 15.0 y)
+                             (* 3.0 z)))
+               (denom-ref (+ d65-ref-x
+                             (* 15.0 d65-ref-y)
+                             (* 3.0 d65-ref-z)))
+               (u (* L 13.0 4.0
+                     (- (/ x denom-val)
+                        (/ d65-ref-x denom-ref))))
+               (v (* L 13.0 9.0
+                     (- (/ y denom-val)
+                        (/ d65-ref-y denom-ref)))))
+          (list :luv L u v)))
+        (error "Expected a XYZ triplet")))
 
 (defun luv-to-xyz (luv)
-  (cl-destructuring-bind (L u v) luv
-    (let* ((inv-denom-ref (/ 1.0
-                             (+ d65-ref-x
-                                (* 15.0 d65-ref-y)
-                                (* 3.0 d65-ref-z))))
-           (u-ref (* 4.0 d65-ref-x inv-denom-ref))
-           (v-ref (* 9.0 d65-ref-y inv-denom-ref))
-           (y (if (> L (* cie-kappa cie-epsilon))
-                  (expt (/ (+ L 16.0) 116.0) 3.0)
-                (/ L cie-kappa)))
-           (a (/ (- (/ (* 52.0 L)
-                       (+ u (* 13.0 L u-ref)))
-                    1.0)
-                 3.0))
-           (b (* -5.0 y))
-           (c (/ -1.0 3.0))
-           (d (* y
-                 (- (/ (* 39.0 L)
-                       (+ v (* 13.0 L v-ref)))
-                    5.0)))
-           (x (/ (- d b)
-                 (- a c)))
-           (z (+ (* a x) b)))
-      (list x y z))))
+  (if (eq (car luv) :luv)
+      (cl-destructuring-bind (L u v) (cdr luv)
+        (let* ((inv-denom-ref (/ 1.0
+                                 (+ d65-ref-x
+                                    (* 15.0 d65-ref-y)
+                                    (* 3.0 d65-ref-z))))
+               (u-ref (* 4.0 d65-ref-x inv-denom-ref))
+               (v-ref (* 9.0 d65-ref-y inv-denom-ref))
+               (y (if (> L (* cie-kappa cie-epsilon))
+                      (expt (/ (+ L 16.0) 116.0) 3.0)
+                    (/ L cie-kappa)))
+               (a (/ (- (/ (* 52.0 L)
+                           (+ u (* 13.0 L u-ref)))
+                        1.0)
+                     3.0))
+               (b (* -5.0 y))
+               (c (/ -1.0 3.0))
+               (d (* y
+                     (- (/ (* 39.0 L)
+                           (+ v (* 13.0 L v-ref)))
+                        5.0)))
+               (x (/ (- d b)
+                     (- a c)))
+               (z (+ (* a x) b)))
+          (list :xyz x y z)))
+    (error "Expected a Luv triplet")))
+
+;;;; Conversions between Luv and LCH(uv) spaces
 
 (defun luv-to-lch-uv (luv)
-  (cl-destructuring-bind (L u v) luv
-    (let* ((C (sqrt (+ (* u u) (* v v))))
-           (H (mod (* 180.0 (/ (atan v u) pi)) 3)))
-      (list L C H))))
+  (if (eq (car luv) :luv)
+      (cl-destructuring-bind (L u v) (cdr luv)
+        (let* ((C (sqrt (+ (* u u) (* v v))))
+               (H (mod (* 180.0 (/ (atan v u) pi)) 360.0)))
+          (list :lch-uv L C H)))
+    (error "Expected a Luc triplet")))
 
 (defun lch-uv-to-luv (lch)
-  (cl-destructuring-bind (L C H) lch
-    (let* ((h (/ (* pi H) 180.0))
-           (u (* C (cos h)))
-           (v (* C (sin h))))
-      (list L u v))))
+  (if (eq (car lch) :lch-uv)
+      (cl-destructuring-bind (L C H) (cdr lch)
+        (let* ((h (/ (* pi H) 180.0))
+               (u (* C (cos h)))
+               (v (* C (sin h))))
+          (list :luv L u v)))
+    (error "Expected a LCH(uv) triplet")))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Derived conversions between color spaces
+;;;; Transitive conversions between spaces
 
-(defun srgb-to-lch-uv (rgb)
-  (luv-to-lch-uv
-   (xyz-to-luv
-    (srgb-to-xyz rgb))))
+(defun to-xyz (color)
+  (if (stringp color)
+      (srgb-to-xyz (string-to-rgb color))
+    (case (car color)
+      (:xyz color)
+      (:rgb (srgb-to-xyz color))
+      (:luv (luv-to-xyz color))
+      (:lch-uv (luv-to-xyz (lch-uv-to-luv color))))))
 
-(defun lch-uv-to-srgb (lch)
-  (xyz-to-srgb
-   (luv-to-xyz
-    (lch-uv-to-luv lch))))
+(defmacro unless-in-space (color space &rest body)
+  (declare (indent 2))
+  (let ((color-sym (cl-gensym "color")))
+    `(let ((,color-sym ,color))
+       (or (and (consp ,color-sym)
+                (eq (car ,color-sym) ,space)
+                ,color-sym)
+           ,@body))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun to-rgb (color)
+  (unless-in-space color :rgb
+    (xyz-to-srgb (to-xyz color))))
+
+(defun to-luv (color)
+  (unless-in-space color :luv
+    (xyz-to-luv (to-xyz color))))
+
+(defun to-lch-uv (color)
+  (unless-in-space color :lch-uv
+    (luv-to-lch-uv (to-luv color))))
+
+(defun color-to-string (color)
+  (if (stringp color)
+      color
+    (apply #'concat
+           "#"
+           (mapcar (lambda (v)
+                     (let ((i (max 0 (min 255 (round (* 255.0 v))))))
+                       (format "%02X" i)))
+                   (cdr (to-rgb color))))))
+
 ;;;; Interpolating inside color spaces
 
 (defun lin-interp (from to alpha)
@@ -203,11 +244,15 @@
     (mod (lin-interp from to-act alpha) 360.0)))
 
 (defun lch-interp (from to alpha)
-  (cl-destructuring-bind (from-L from-C from-H) from
-    (cl-destructuring-bind (to-L to-C to-H) to
-      (list (lin-interp from-L to-L alpha)
-            (lin-interp from-C to-C alpha)
-            (ang-interp from-H to-H alpha)))))
+  (if (and (eq (car from) :lch-uv)
+           (eq (car to) :lch-uv))
+      (cl-destructuring-bind (from-L from-C from-H) (cdr from)
+        (cl-destructuring-bind (to-L to-C to-H) (cdr to)
+          (list :lch-uv
+                (lin-interp from-L to-L alpha)
+                (lin-interp from-C to-C alpha)
+                (ang-interp from-H to-H alpha))))
+    (error "Expected LCH(uv) triplets")))
 
 (defun linspace (from to steps)
   (when (> steps 1)
@@ -225,14 +270,13 @@
 ;;;; Random testing stuff
 
 (defun color-interp (from to steps)
-  (let* ((from-lch (srgb-to-lch-uv (string-to-rgb from)))
-         (to-lch (srgb-to-lch-uv (string-to-rgb to))))
+  (let* ((from-lch (to-lch-uv from))
+         (to-lch (to-lch-uv to)))
     (mapcar (lambda (alpha)
-              (rgb-to-string
-               (lch-uv-to-srgb
-                (lch-interp from-lch
-                            to-lch
-                            alpha))))
+              (color-to-string
+               (lch-interp from-lch
+                           to-lch
+                           alpha)))
             (linspace 0.0 1.0 steps))))
 
 (provide 'liquorice-tool)
