b7e0d8b057bd875f898a8d450a86b549bede7548
Matti Hänninen <matti@mjhanninen.com>
2015-05-10 07:02:11 +0300

    Clean up code

--- liquorice-tool.el
+++ liquorice-tool.el
@@ -44,9 +44,7 @@
 ;;;; String conversions
 
 (defun rgb-to-string (rgb)
-  (let ((r (car rgb))
-        (g (cadr rgb))
-        (b (caddr rgb)))
+  (cl-destructuring-bind (r g b) rgb
     (liquorice-rgb r g b)))
 
 (defun hex-to-float (h)
@@ -94,107 +92,94 @@
     (* 12.92 v)))
 
 (defun srgb-to-xyz (rgb)
-  (let* ((r (srgb-compand (car rgb)))
-         (g (srgb-compand (cadr rgb)))
-         (b (srgb-compand (caddr rgb)))
-         (x (+ (* r 0.4124564)
-               (* g 0.3575761)
-               (* b 0.1804375)))
-         (y (+ (* r 0.2126729)
-               (* g 0.7151522)
-               (* b 0.0721750)))
-         (z (+ (* r 0.0193339)
-               (* g 0.1191920)
-               (* b 0.9503041))))
-    (list x y z)))
+  (cl-destructuring-bind (r g b) (mapcar #'srgb-compand rgb)
+    (let* ((x (+ (* r 0.4124564)
+                 (* g 0.3575761)
+                 (* b 0.1804375)))
+           (y (+ (* r 0.2126729)
+                 (* g 0.7151522)
+                 (* b 0.0721750)))
+           (z (+ (* r 0.0193339)
+                 (* g 0.1191920)
+                 (* b 0.9503041))))
+      (list x y z))))
 
 (defun xyz-to-srgb (xyz)
-  (let* ((x (car xyz))
-         (y (cadr xyz))
-         (z (caddr xyz))
-         (r (srgb-expand
-             (+ (* x  3.2404542)
-                (* y -1.5371385)
-                (* z -0.4985314))))
-         (g (srgb-expand
-             (+ (* x -0.9692660)
-                (* y  1.8760108)
-                (* z  0.0415560))))
-         (b (srgb-expand
-             (+ (* x  0.0556434)
-                (* y -0.2040259)
-                (* z  1.0572252)))))
-    (list r g b)))
+  (cl-destructuring-bind (x y z) xyz
+    (let* ((r (srgb-expand
+               (+ (* x  3.2404542)
+                  (* y -1.5371385)
+                  (* z -0.4985314))))
+           (g (srgb-expand
+               (+ (* x -0.9692660)
+                  (* y  1.8760108)
+                  (* z  0.0415560))))
+           (b (srgb-expand
+               (+ (* x  0.0556434)
+                  (* y -0.2040259)
+                  (* z  1.0572252)))))
+      (list r g b))))
 
 (defun xyz-to-luv (xyz)
-  (let* ((x (car xyz))
-         (y (cadr xyz))
-         (z (caddr xyz))
-         (y-rel (/ y d65-ref-y))
-         (L (if (> y-rel cie-epsilon)
-                (- (* 116.0
-                      (expt y-rel (/ 1.0 3.0)))
-                   16.0)
-              (* cie-kappa y-rel)))
-         (denom-val (+ x
-                       (* 15.0 y)
-                       (* 3.0 z)))
-         (denom-ref (+ d65-ref-x
-                       (* 15.0 d65-ref-y)
-                       (* 3.0 d65-ref-z)))
-         (u (* L 13.0 4.0
-               (- (/ x denom-val)
-                  (/ d65-ref-x denom-ref))))
-         (v (* L 13.0 9.0
-               (- (/ y denom-val)
-                  (/ d65-ref-y denom-ref)))))
-    (list L u v)))
+  (cl-destructuring-bind (x y z) xyz
+    (let* ((y-rel (/ y d65-ref-y))
+           (L (if (> y-rel cie-epsilon)
+                  (- (* 116.0
+                        (expt y-rel (/ 1.0 3.0)))
+                     16.0)
+                (* cie-kappa y-rel)))
+           (denom-val (+ x
+                         (* 15.0 y)
+                         (* 3.0 z)))
+           (denom-ref (+ d65-ref-x
+                         (* 15.0 d65-ref-y)
+                         (* 3.0 d65-ref-z)))
+           (u (* L 13.0 4.0
+                 (- (/ x denom-val)
+                    (/ d65-ref-x denom-ref))))
+           (v (* L 13.0 9.0
+                 (- (/ y denom-val)
+                    (/ d65-ref-y denom-ref)))))
+      (list L u v))))
 
 (defun luv-to-xyz (luv)
-  (let* ((L (car luv))
-         (u (cadr luv))
-         (v (caddr luv))
-
-         (inv-denom-ref (/ 1.0
-                           (+ d65-ref-x
-                              (* 15.0 d65-ref-y)
-                              (* 3.0 d65-ref-z))))
-         (u-ref (* 4.0 d65-ref-x inv-denom-ref))
-         (v-ref (* 9.0 d65-ref-y inv-denom-ref))
-         (y (if (> L (* cie-kappa cie-epsilon))
-                (expt (/ (+ L 16.0) 116.0) 3.0)
-              (/ L cie-kappa)))
-         (a (/ (- (/ (* 52.0 L)
-                     (+ u (* 13.0 L u-ref)))
-                  1.0)
-               3.0))
-         (b (* -5.0 y))
-         (c (/ -1.0 3.0))
-         (d (* y
-               (- (/ (* 39.0 L)
-                     (+ v (* 13.0 L v-ref)))
-                  5.0)))
-         (x (/ (- d b)
-               (- a c)))
-         (z (+ (* a x) b)))
-    (list x y z)))
+  (cl-destructuring-bind (L u v) luv
+    (let* ((inv-denom-ref (/ 1.0
+                             (+ d65-ref-x
+                                (* 15.0 d65-ref-y)
+                                (* 3.0 d65-ref-z))))
+           (u-ref (* 4.0 d65-ref-x inv-denom-ref))
+           (v-ref (* 9.0 d65-ref-y inv-denom-ref))
+           (y (if (> L (* cie-kappa cie-epsilon))
+                  (expt (/ (+ L 16.0) 116.0) 3.0)
+                (/ L cie-kappa)))
+           (a (/ (- (/ (* 52.0 L)
+                       (+ u (* 13.0 L u-ref)))
+                    1.0)
+                 3.0))
+           (b (* -5.0 y))
+           (c (/ -1.0 3.0))
+           (d (* y
+                 (- (/ (* 39.0 L)
+                       (+ v (* 13.0 L v-ref)))
+                    5.0)))
+           (x (/ (- d b)
+                 (- a c)))
+           (z (+ (* a x) b)))
+      (list x y z))))
 
 (defun luv-to-lch-uv (luv)
-  (let* ((L (car luv))
-         (u (cadr luv))
-         (v (caddr luv))
-         (C (sqrt (+ (* u u) (* v v))))
-         (H (mod (* 180.0 (/ (atan v u) pi)) 3)))
-    (list L C H)))
+  (cl-destructuring-bind (L u v) luv
+    (let* ((C (sqrt (+ (* u u) (* v v))))
+           (H (mod (* 180.0 (/ (atan v u) pi)) 3)))
+      (list L C H))))
 
 (defun lch-uv-to-luv (lch)
-  (let* ((L (car lch))
-         (C (cadr lch))
-         (H (caddr lch))
-         (h (/ (* pi H) 180.0))
-         (u (* C (cos h)))
-         (v (* C (sin h))))
-    (list L u v)))
+  (cl-destructuring-bind (L C H) lch
+    (let* ((h (/ (* pi H) 180.0))
+           (u (* C (cos h)))
+           (v (* C (sin h))))
+      (list L u v))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Derived conversions between color spaces
@@ -226,16 +211,11 @@
     (mod (lin-interp from to-act alpha) 360.0)))
 
 (defun lch-interp (from to alpha)
-  (let* ((from-L (car from))
-         (from-C (cadr from))
-         (from-H (caddr from))
-         (to-L (car to))
-         (to-C (cadr to))
-         (to-H (caddr to))
-         (L (lin-interp from-L to-L alpha))
-         (C (lin-interp from-C to-C alpha))
-         (H (ang-interp from-H to-H alpha)))
-    (list L C H)))
+  (cl-destructuring-bind (from-L from-C from-H) from
+    (cl-destructuring-bind (to-L to-C to-H) to
+      (list (lin-interp from-L to-L alpha)
+            (lin-interp from-C to-C alpha)
+            (ang-interp from-H to-H alpha)))))
 
 (defun linspace (from to steps)
   (when (> steps 1)
