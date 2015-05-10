39101be2ca617bf0711c6f93818d567c9097699b
Matti Hänninen <matti@mjhanninen.com>
2015-05-10 06:42:56 +0300

    Add copyright notices and note about status

--- liquorice-tool.el
+++ liquorice-tool.el
@@ -1,3 +1,25 @@
+;;; liquorice-tool.el -- an utility package for Liquorice library
+;;; Copyright (C) 2015 Matti Hänninen <matti@mjhanninen.com>
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;; WORK IN PROGRESS
+
+;;; Please do note that this files is in work-in-progress condition and in its
+;;; current state should be considered entirely in as unstable of internal to
+;;; the library.
+
 (provide 'liquorice-tool)
 
 ;;; References:
