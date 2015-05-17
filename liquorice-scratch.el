335e71e632d9fdf59cbb1167282166c129044f5a
Matti Hänninen <matti@mjhanninen.com>
2015-05-17 11:08:28 +0300

    Show more info about color in sample strips
    Amend the tools in scratch package so that they print out more
    information about the color samples. Makes debugging easier.

--- liquorice-scratch.el
+++ liquorice-scratch.el
@@ -1,16 +1,27 @@
 (require 'liquorice-color)
 (require 'liquorice-math)
 
+(defun insert-color (color)
+  (let ((color-str (liquorice-to-string color)))
+    (insert color-str
+            " ("
+            (let ((sample " f "))
+              (put-text-property 0 3
+                                 'face `(:background ,color-str)
+                                 sample)
+              sample)
+            ") "
+            (format "(%s%s)"
+                    (car color)
+                    (apply #'concat
+                           (mapcar (lambda (x)
+                                     (format " %.3f" x))
+                                   (cdr color)))))))
+
 (defun insert-color-strip (&rest colors)
-  (mapc (lambda (color)
-          (let ((s "   "))
-            (put-text-property 0 3
-                               'face `(:background
-                                       ,(liquorice-to-string color))
-                               s)
-            (insert s)))
-        colors)
-  (insert "\n"))
+  (dolist (color colors)
+    (insert-color color)
+    (insert "\n")))
 
 (defun insert-color-ramp (from-color to-color steps)
   (apply #'insert-color-strip
